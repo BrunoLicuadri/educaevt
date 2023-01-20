c055d9fa8cdb5a1f8b8e80aa045b7207df349ced
BrunoLicuadri <blicuadri@gmail.com>
2023-01-20 11:00:36 -0300

    Criamos a entidade Bloco e seu relacionamento com Atividade

--- src/main/java/com/godknows/educaevt/entities/Atividade.java
+++ src/main/java/com/godknows/educaevt/entities/Atividade.java
@@ -2,6 +2,9 @@ package com.godknows.educaevt.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="tb_atividade")
 public class Atividade {
@@ -10,12 +13,17 @@ public class Atividade {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @Column(columnDefinition = "TEXT")
     private String descricao;
     private Double preco;
 
     @ManyToOne
     @JoinColumn(name="categoria_id")
     private Categoria categoria;
+
+    @OneToMany(mappedBy="atividade")
+    private List<Bloco> blocos = new ArrayList<>();
 
     public Atividade(){
 
@@ -62,5 +70,9 @@ public class Atividade {
 
     public Categoria getCategoria() {
         return categoria;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
     }
 }
